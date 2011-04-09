ac80ed11dfdeaf5e034c45a53ffa70be398c9fd4
zhlinl <>
2011-04-09 14:47:21 +0000

    *** empty log message ***

---
+++ PKUTutorial/test/config_tri.py
@@ -0,0 +1,4 @@
+DataNames = { 'JPsi_DATA_Trigger' : ('J#Psi#rightarrow#mu#mu DATA', 'JPsi_DATA') }
+
+JPsi_DATA = { 'tagAndProbe/L1DoubleMuOpen_Tight/fit_eff': ( 'Trigger', 'L1DoubleMuOpen', 0) }
+ 
