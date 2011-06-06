3662117c478f73f8df4f0b59b795cc09b8052a25
zhlinl <>
2011-06-06 10:13:55 +0000

    *** empty log message ***

--- Polarization/jpsi/runPrepareTree.sh
+++ Polarization/jpsi/runPrepareTree.sh
@@ -5,6 +5,7 @@ source /afs/ihep.ac.cn/users/z/zhangll/workspace/rootset.sh 9
 mapdir=/publicfs/cms/user/zhangll/data/polarization/
 dirstruct=/afs/ihep.ac.cn/users/z/zhangll/workspace/work/polarization/Polarization/
 project=${dirstruct}project/
+cd ${dirstruct}
 
 #python PrepareFitTree.py --output=treeFall10_test.root	--mc  ${mapdir}TTree_pol_noTriggerFilter_MCPromptFall10_23Mar2011.root
 
